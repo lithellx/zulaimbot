e3d3eba7da4c806a33ef6d9e3cc1da40f7c78b3d
lithé <45209073+lithell@users.noreply.github.com>
2020-01-16 15:24:24 +0300

    Add files via upload

---
+++ ZulaAimbot/fonksiyonlar.h
@@ -0,0 +1,94 @@
+#pragma warning(disable: 4244)
+#pragma warning(disable: 4996)
+bool Create;
+
+LPD3DXLINE	 Line1;
+LPD3DXLINE   Line2;
+LPD3DXLINE   Line3;
+D3DVIEWPORT9 viewport;
+float ScreenCenterX;
+float ScreenCenterY;
+
+float AimHhead = 28.0f;
+float AimHbody = 0.0f;
+float AimHlegs = -28.0f;
+struct ModelPlayer2
+{
+	D3DXVECTOR3 Player;
+	float CrossDist;
+};
+std::vector<ModelPlayer2*>cPlayerA;
+
+float GetDistance(float Xx, float Yy, float xX, float yY)
+{
+	return sqrt((yY - Yy) * (yY - Yy) + (xX - Xx) * (xX - Xx));
+}
+int aimsmooth = 2;
+int aimfov = 60;
+
+#define GWL_WNDPROC         (-4)
+#define HOOK(func,addy)	o##func = (t##func)DetourFunction((PBYTE)addy,(PBYTE)hk##func)
+#define ES	0
+#define DIP	1
+#define SSS	2
+unsigned int	uiStride = NULL;
+
+typedef HRESULT(WINAPI *tPresent)(LPDIRECT3DDEVICE9 pDevice, const RECT *a, const RECT *b, HWND c, const RGNDATA *d);
+tPresent oPresent;
+
+typedef HRESULT(WINAPI* tSetStreamSource)(LPDIRECT3DDEVICE9 pDevice, UINT StreamNumber, IDirect3DVertexBuffer9* pStreamData, UINT OffsetInBytes, UINT Stride);
+tSetStreamSource oSetStreamSource;
+
+typedef HRESULT(WINAPI* tEndScene)(LPDIRECT3DDEVICE9 pDevice);
+tEndScene oEndScene;
+
+typedef HRESULT(WINAPI* tDrawIndexedPrimitive)(LPDIRECT3DDEVICE9 pDevice, D3DPRIMITIVETYPE PrimType, INT BaseVertexIndex, UINT MinVertexIndex, UINT NumVertices, UINT startIndex, UINT primCount);
+tDrawIndexedPrimitive oDrawIndexedPrimitive;
+
+extern LRESULT ImGui_ImplWin32_WndProcHandler(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam);
+typedef LRESULT(CALLBACK* WNDPROC)(HWND, UINT, WPARAM, LPARAM);
+
+VOID CreateDevice(DWORD *dwVTable)
+{
+	LPDIRECT3D9 pD3d9;
+	LPDIRECT3DDEVICE9 pD3DDevice;
+	pD3d9 = Direct3DCreate9(D3D_SDK_VERSION);
+	if (pD3d9 == NULL)
+		return;
+	D3DPRESENT_PARAMETERS pPresentParms;
+	ZeroMemory(&pPresentParms, sizeof(pPresentParms));
+	pPresentParms.Windowed = TRUE;
+	pPresentParms.BackBufferFormat = D3DFMT_UNKNOWN;
+	pPresentParms.SwapEffect = D3DSWAPEFFECT_DISCARD;
+	if (FAILED(pD3d9->CreateDevice(D3DADAPTER_DEFAULT, D3DDEVTYPE_HAL, GetDesktopWindow(), D3DCREATE_SOFTWARE_VERTEXPROCESSING, &pPresentParms, &pD3DDevice)))
+		return;
+	DWORD *dwTable = (DWORD *)pD3DDevice;
+	dwTable = (DWORD *)dwTable[0];
+	dwVTable[0] = dwTable[16];
+	dwVTable[1] = dwTable[17];
+}
+
+VOID *DetourCreate(BYTE *src, CONST BYTE *dst, CONST INT len)
+{
+	BYTE *jmp = (BYTE *)malloc(len + 5);
+	DWORD dwBack;
+	VirtualProtect(src, len, PAGE_EXECUTE_READWRITE, &dwBack);
+	memcpy(jmp, src, len);
+	jmp += len;
+	jmp[0] = 0xE9;
+	*(DWORD *)(jmp + 1) = (DWORD)(src + len - jmp) - 5;
+	src[0] = 0xE9;
+	*(DWORD *)(src + 1) = (DWORD)(dst - src) - 5;
+	for (INT i = 5; i < len; i++)
+		src[i] = 0x90;
+	VirtualProtect(src, len, dwBack, &dwBack);
+	return(jmp - len);
+}
+
+DWORD ESADDR = (DWORD)GetModuleHandleA("acknex.dll") + 0x3A74A; // endscene
+DWORD RETES = (ESADDR + 0x8);
+
+DWORD dxAddr = (DWORD)GetModuleHandleA("d3dx9_42.dll") + 0x4DEB0; // all players
+DWORD RetDX = (dxAddr + 0x5);
+
+// WOX AREA / W4H9LT & UNKNOWN CHEATS / MO1RA
---
+++ ZulaAimbot/main.cpp
@@ -0,0 +1,178 @@
+#include <Windows.h>
+#include <d3d9.h>
+#include <d3dx9.h>
+#include <iostream>
+#include <fstream>
+#include <detours.h>
+#include <vector>
+#include "fonksiyonlar.h"
+
+#pragma comment (lib, "d3dx9.lib")
+#pragma comment (lib, "d3d9.lib")
+
+using namespace std;
+
+HRESULT WINAPI hkPresent(LPDIRECT3DDEVICE9 pDevice, const RECT *a, const RECT *b, HWND c, const RGNDATA *d)
+{
+	__asm PUSHAD;
+
+	ScreenCenterX = viewport.Width / 2.0f;
+	ScreenCenterY = viewport.Height / 2.0f;
+
+/*
+		CrossHair(pDevice, Items.d3dcolor8);
+
+		Circle(ScreenCenterX, ScreenCenterY, 60, 60, #COLOR);
+*/
+
+	__asm POPAD;
+	return oPresent(pDevice, a, b, c, d);
+}
+
+HRESULT WINAPI hkEndScene(LPDIRECT3DDEVICE9 pDevice)
+{
+	if (cPlayerA.size() != NULL)
+	{
+		UINT BestTarget = -1;
+		DOUBLE fClosestPos = 99999;
+		for (size_t i = 0; i < cPlayerA.size(); i += 1)
+		{
+			float radiusx = aimfov * (ScreenCenterX / 100);
+			float radiusy = aimfov * (ScreenCenterY / 100);
+
+			cPlayerA[i]->CrossDist = GetDistance(cPlayerA[i]->Player.x, cPlayerA[i]->Player.y, ScreenCenterX, ScreenCenterY);
+			if (cPlayerA[i]->Player.x >= ScreenCenterX - radiusx &&
+				cPlayerA[i]->Player.x <= ScreenCenterX + radiusx &&
+				cPlayerA[i]->Player.y >= ScreenCenterY - radiusy &&
+				cPlayerA[i]->Player.y <= ScreenCenterY + radiusy)
+				if (cPlayerA[i]->CrossDist < fClosestPos)
+				{
+					fClosestPos = cPlayerA[i]->CrossDist;
+					BestTarget = i;
+				}
+		}
+		if (BestTarget != -1)
+		{
+			double DistX = (double)cPlayerA[BestTarget]->Player.x - viewport.Width / 2.0f;
+			double DistY = (double)cPlayerA[BestTarget]->Player.y - viewport.Height / 2.0f;
+
+			aimsmooth /= DistX;
+			aimsmooth /= DistY;
+
+			if (GetAsyncKeyState(VK_SHIFT) & 0x8000) {
+				mouse_event(MOUSEEVENTF_MOVE, DistX, DistY, NULL, NULL);
+			}
+		}
+		cPlayerA.clear();
+	}
+	return oEndScene(pDevice);
+}
+
+HRESULT WINAPI hkDrawIndexedPrimitive(LPDIRECT3DDEVICE9 pDevice, D3DPRIMITIVETYPE PrimType, INT BaseVertexIndex, UINT MinVertexIndex, UINT NumVertices, UINT startIndex, UINT PrimitiveCount)
+{
+	__asm nop
+	HRESULT hRet = oDrawIndexedPrimitive(pDevice, PrimType, BaseVertexIndex, MinVertexIndex, NumVertices, startIndex, PrimitiveCount);
+
+	return hRet;
+}
+
+void W2AIM(LPDIRECT3DDEVICE9 pDevice, float AimH)
+{
+	D3DXMATRIX projection, view, world;
+	ModelPlayer2 *pPlayer = new ModelPlayer2;
+	D3DXVECTOR3 pOut(0, AimH, 0), pV(0, AimH, 0);
+	pDevice->GetViewport(&viewport);
+	pDevice->GetTransform(D3DTS_PROJECTION, &projection);
+	pDevice->GetTransform(D3DTS_VIEW, &view);
+	pDevice->GetTransform(D3DTS_WORLD, &world);
+	D3DXVec3Project(&pOut, &pV, &viewport, &projection, &view, &world);
+	if (pOut.z < 1)
+	{
+		pPlayer->Player.x = pOut.x;
+		pPlayer->Player.y = pOut.y;
+	}
+	cPlayerA.push_back(pPlayer);
+}
+
+_declspec (naked) HRESULT WINAPI playersAll()
+{
+	static LPDIRECT3DDEVICE9 pDevice;
+	__asm
+	{
+		xor ebx, ebx
+		imul edx, edx, 03
+		mov dword ptr ds : [pDevice], eax
+		pushad
+	}
+
+	W2AIM(pDevice, AimHhead);
+
+/*
+	W2AIM(pDevice, AimHbody);
+
+	W2AIM(pDevice, AimHlegs);
+*/
+	__asm
+	{
+		popad
+		jmp RetDX
+	}
+}
+
+HRESULT WINAPI hkSetStreamSource(LPDIRECT3DDEVICE9 pDevice, UINT StreamNumber, IDirect3DVertexBuffer9* pStreamData, UINT OffsetInBytes, UINT Stride)
+{
+	__asm nop
+	if (StreamNumber == 0) {
+		uiStride = Stride;
+	}
+	return oSetStreamSource(pDevice, StreamNumber, pStreamData, OffsetInBytes, Stride);
+}
+
+LRESULT CALLBACK MsgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam) { return DefWindowProc(hwnd, uMsg, wParam, lParam); }
+void DX_Init(DWORD* table)
+{
+	WNDCLASSEX wc = { sizeof(WNDCLASSEX), CS_CLASSDC, MsgProc, 0L, 0L, GetModuleHandle(NULL), NULL, NULL, NULL, NULL, "DX", NULL };
+	RegisterClassEx(&wc);
+	HWND hWnd = CreateWindow("DX", NULL, WS_OVERLAPPEDWINDOW, 100, 100, 300, 300, GetDesktopWindow(), NULL, wc.hInstance, NULL);
+	LPDIRECT3D9 pD3D = Direct3DCreate9(D3D_SDK_VERSION);
+	D3DPRESENT_PARAMETERS d3dpp;
+	ZeroMemory(&d3dpp, sizeof(d3dpp));
+	d3dpp.Windowed = TRUE;
+	d3dpp.SwapEffect = D3DSWAPEFFECT_DISCARD;
+	d3dpp.BackBufferFormat = D3DFMT_UNKNOWN;
+	LPDIRECT3DDEVICE9 pd3dDevice;
+	pD3D->CreateDevice(D3DADAPTER_DEFAULT, D3DDEVTYPE_HAL, hWnd, D3DCREATE_SOFTWARE_VERTEXPROCESSING, &d3dpp, &pd3dDevice);
+	DWORD* pVTable = (DWORD*)pd3dDevice;
+	pVTable = (DWORD*)pVTable[0];
+	table[ES] = pVTable[42];
+	table[DIP] = pVTable[82];
+	table[SSS] = pVTable[100];
+	DestroyWindow(hWnd);
+}
+
+DWORD WINAPI Thread(LPVOID)
+{
+	DWORD VTable[3] = { 0 };
+	while (GetModuleHandle("d3d9.dll") == NULL) {
+		Sleep(250);
+	}
+	DWORD dwVTable[2] = { 0 };
+	CreateDevice(dwVTable);
+	oPresent = (tPresent)DetourCreate((PBYTE)dwVTable[1], (PBYTE)&hkPresent, 5);
+	DX_Init(VTable);
+	HOOK(EndScene, VTable[ES]);
+	HOOK(DrawIndexedPrimitive, VTable[DIP]);
+	HOOK(SetStreamSource, VTable[SSS]);
+	DetourCreate((PBYTE)dxAddr, (PBYTE)playersAll, 5);
+	return 0;
+}
+
+BOOL WINAPI DllMain(HMODULE hModule, DWORD dwReason, LPVOID lpvReserved)
+{
+	if (dwReason == DLL_PROCESS_ATTACH) {
+		CreateThread(0, 0, Thread, 0, 0, 0);
+	}
+	return TRUE;
+}
+
+// UNKNOWN CHEATS / MO1RA
